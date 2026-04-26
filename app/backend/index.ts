6ff68ffce0bb22b3639de18f89ba4aac826eebfb
Jatin Singh Patwal <jatinpatwal011@gmail.com>
2026-04-26 14:52:51 +0530

    first commit

--- app/backend/index.ts
+++ app/backend/index.ts
@@ -3,3 +3,13 @@ import cors from 'cors'
 import 'dotenv/config'
 
 const app = express()
+const port = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port,()=>console.log("Server Started",port))
