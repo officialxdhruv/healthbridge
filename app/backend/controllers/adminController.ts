4548b12db281322d2de73112b8883301f9d40bde
Jatin Singh Patwal <jatinpatwal011@gmail.com>
2026-04-26 17:54:15 +0530

    Only little work done

---
+++ app/backend/controllers/adminController.ts
@@ -0,0 +1,13 @@
+
+const addDoctor = async(req,res) =>{
+    try {
+
+        const {name,email,password,speciality,degree,experience,about,fees,address} = req.body
+        
+    
+    } catch (error) {
+        
+    }
+}
+
+export {addDoctor}
